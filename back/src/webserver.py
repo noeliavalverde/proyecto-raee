20d6cf5a024497009ef214aa684b80dc58f757b8
noeliavm <noelia.valverde@gmail.com>
2022-05-25 19:58:59 +0200

    TASK - endpoints register, diagnostic-in, diagnostic-out, repair-in, repair-out, test-in, test-out documentation completed

--- back/src/webserver.py
+++ back/src/webserver.py
@@ -24,13 +24,13 @@ def create_app(repositories):
     @app.route("/api/process/register", methods=["POST"])
     def register_machine_event():
 
-        """Endpoint to save a register data of machine
-        This is using docstrings for specifications.
+        """Endpoint to save the registration details of a machine
+
         ---
         parameters:
             -   in: body
-                name: event
-                description: The event to send
+                name: Register event
+                description: Event to register a machine
                 schema:
                     type: object
                     required:
@@ -42,20 +42,22 @@ def create_app(repositories):
                     properties:
                         id_machine:
                             type: uuid
-                            description: A unique id for the machine
-                            example: 125489621
+                            description: An unique id for the machine
+                            example: flgr0087
                         employee:
                             type: string
                             description: An employee id
                             example: operario-005
                         timestamp:
-                            type: date
+                            type: string
                             description: Register date in isoformat
-                            example: 2022-05-25
+                            example: "2022-05-25 00:00:00"
+                            pattern: YY-MM-DD hh:mm:ss
                         event:
                             type: string
                             description: Specific event
                             example: register
+                            default: register
                         payload:
                             type: object
                             description: An object containing brand and model of the machine
@@ -87,6 +89,56 @@ def create_app(repositories):
 
     @app.route("/api/process/diagnostic/enter", methods=["POST"])
     def diagnostic_machine_enter():
+        """Endpoint to register the timestamp when a machine gets into diagnostic process
+
+        ---
+        parameters:
+            -   in: body
+                name: event
+                description: Enter timestamp of a machine at diagnostic process.
+                schema:
+                    type: object
+                    required:
+                        - id_machine
+                        - employee
+                        - timestamp
+                        - event
+                    properties:
+                        id_machine:
+                            type: uuid
+                            description: A unique id for the machine
+                            example: flgr0087
+                        employee:
+                            type: string
+                            description: An employee id
+                            example: operario-005
+                        timestamp:
+                            type: string
+                            description: Date in isoformat
+                            example: "2022-05-25 00:00:00"
+                            pattern: YY-MM-DD hh:mm:ss
+                        event:
+                            type: string
+                            description: Specific event
+                            example: diagnostic_in
+                            default: diagnostic_in
+                        payload:
+                            type: object
+                            description:
+                            example: {docs: }
+                            items:
+                                docs:
+                                    type:string
+
+
+
+        responses:
+          200:
+            description: Returns a 200 response. Diagnostic in event registered.
+
+
+        """
+
         data = request.json
         event = Event(
             id_machine=data["id_machine"],
@@ -99,10 +151,54 @@ def create_app(repositories):
         washer_event = repositories["event"].save_event(event)
         return "", 200
 
-
-
     @app.route("/api/process/diagnostic/exit", methods=["POST"])
     def diagnostic_machine_exit():
+        """Endpoint to register the timestamp when a machine gets out of diagnostic process. It also registers the next process that the machine needs, repair, test, valid, detachable
+        ---
+        parameters:
+            -   in: body
+                name: event
+                description: Exit timestamp of a machine at diagnostic process.
+                schema:
+                    type: object
+                    required:
+                        - id_machine
+                        - employee
+                        - timestamp
+                        - event
+                        - payload
+                    properties:
+                        id_machine:
+                            type: uuid
+                            description: A unique id for the machine
+                            example: flgr0087
+                        employee:
+                            type: string
+                            description: An employee id
+                            example: operario-005
+                        timestamp:
+                            type: string
+                            description: Date in isoformat
+                            example: "2022-05-25 00:00:00"
+                            pattern: YY-MM-DD hh:mm:ss
+                        event:
+                            type: string
+                            description: Specific event
+                            example: diagnostic_out
+                            default: diagnostic_out
+                        payload:
+                            type: object
+                            description: An object containing next step of the process. Must be "repair", "test", "valid" or "detachable"
+                            example: {"next_event": "repair"}
+
+
+        responses:
+          200:
+            description: Returns a 200 response. Registered.
+
+
+        """
+
         data = request.json
         event = Event(
             id_machine=data["id_machine"],
@@ -117,6 +213,51 @@ def create_app(repositories):
 
     @app.route("/api/process/repair/enter", methods=["POST"])
     def repair_machine_enter():
+        """Endpoint to register the timestamp when a machine gets into repair process
+        ---
+        parameters:
+            -   in: body
+                name: event
+                description: Enter timestamp of a machine at repair process.
+                schema:
+                    type: object
+                    required:
+                        - id_machine
+                        - employee
+                        - timestamp
+                        - event
+                        - payload
+                    properties:
+                        id_machine:
+                            type: uuid
+                            description: A unique id for the machine
+                            example: flgr0087
+                        employee:
+                            type: string
+                            description: An employee id
+                            example: operario-005
+                        timestamp:
+                            type: string
+                            description: Date in isoformat
+                            example: "2022-05-25 00:00:00"
+                            pattern: YY-MM-DD hh:mm:ss
+                        event:
+                            type: string
+                            description: Specific event
+                            example: repair_in
+                            default: repair_in
+                        payload:
+                            type: object
+                            example: {}
+
+
+        responses:
+          200:
+            description: Returns a 200 response. Registered.
+
+
+        """
+
         data = request.json
         event = Event(
             id_machine=data["id_machine"],
@@ -131,6 +272,55 @@ def create_app(repositories):
 
     @app.route("/api/process/repair/exit", methods=["POST"])
     def repair_machine_exit():
+
+        """Endpoint to register the timestamp when a machine gets out of repair process
+        ---
+        parameters:
+            -   in: body
+                name: event
+                description: Exit timestamp of a machine at repair process.
+                schema:
+                    type: object
+                    required:
+                        - id_machine
+                        - employee
+                        - timestamp
+                        - event
+                        - payload
+                    properties:
+                        id_machine:
+                            type: uuid
+                            description: A unique id for the machine
+                            example: flgr0087
+                        employee:
+                            type: string
+                            description: An employee id
+                            example: operario-005
+                        timestamp:
+                            type: string
+                            description: Date in isoformat
+                            example: "2022-05-25 00:00:00"
+                            pattern: YY-MM-DD hh:mm:ss
+                        event:
+                            type: string
+                            description: Specific event
+                            example: diagnostic_out
+                            default: diagnostic_out
+                        payload:
+                            type: object
+                            description: An object containing next step of process
+                            example: {"next_event": "test"}
+                            items:
+                                next_event:
+                                    type:string
+
+        responses:
+          200:
+            description: Returns a 200 response. Registered.
+
+
+        """
+
         data = request.json
         event = Event(
             id_machine=data["id_machine"],
@@ -145,6 +335,51 @@ def create_app(repositories):
 
     @app.route("/api/process/test/enter", methods=["POST"])
     def test_machine_enter():
+
+        """Endpoint to register the timestamp when a machine gets into test process
+        ---
+        parameters:
+            -   in: body
+                name: event
+                description: Enter timestamp of a machine at test process.
+                schema:
+                    type: object
+                    required:
+                        - id_machine
+                        - employee
+                        - timestamp
+                        - event
+                        - payload
+                    properties:
+                        id_machine:
+                            type: uuid
+                            description: A unique id for the machine
+                            example: flgr0025
+                        employee:
+                            type: string
+                            description: An employee id
+                            example: operario-005
+                        timestamp:
+                            type: string
+                            description: Date in isoformat
+                            example: "2022-05-25 00:00:00"
+                            pattern: YY-MM-DD hh:mm:ss
+                        event:
+                            type: string
+                            description: Specific event
+                            example: test_in
+                            default: test_in
+                        payload:
+                            type: object
+                            example: {}
+
+
+        responses:
+          200:
+            description: Returns a 200 response. Registered.
+
+
+        """
         data = request.json
         event = Event(
             id_machine=data["id_machine"],
@@ -159,6 +394,52 @@ def create_app(repositories):
 
     @app.route("/api/process/test/exit", methods=["POST"])
     def test_machine_exit():
+
+        """Endpoint to register the timestamp when a machine gets out of test process
+        ---
+        parameters:
+            -   in: body
+                name: event
+                description: Exit timestamp of a machine at test process.
+                schema:
+                    type: object
+                    required:
+                        - id_machine
+                        - employee
+                        - timestamp
+                        - event
+                        - payload
+                    properties:
+                        id_machine:
+                            type: uuid
+                            description: A unique id for the machine
+                            example: flgr0025
+                        employee:
+                            type: string
+                            description: An employee id
+                            example: operario-005
+                        timestamp:
+                            type: string
+                            description: Date in isoformat
+                            example: "2022-05-25 00:00:00"
+                            pattern: YY-MM-DD hh:mm:ss
+                        event:
+                            type: string
+                            description: Specific event
+                            example: test_out
+                            default: test_out
+                        payload:
+                            type: object
+                            example: { "procedures": [
+                                            {"title": "test 1", "is_completed": 0},
+                                            {"title": "test 2", "is_completed": 0},
+                                            ],
+                                        }
+        responses:
+          200:
+            description: Returns a 200 response. Registered.
+
+        """
         data = request.json
         event = Event(
             id_machine=data["id_machine"],
