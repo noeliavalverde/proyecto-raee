36f1ad8612919b2d91a3ba57a921dc80345526bb
noeliavm <noelia.valverde@gmail.com>
2022-06-06 09:14:39 +0200

    FEAT - Validation at register event of not empty id and not existing id

--- back/src/validations_endpoints/validations_general.py
+++ back/src/validations_endpoints/validations_general.py
@@ -1,8 +1,9 @@
 import json
-from src.domain.events import Event
+from src.domain.events import Event, EventRepository
+import sqlite3
 
 
-def validate_register(registration_machine):
+def validate_register_contains_brand_and_model(registration_machine):
 
     event_dict = registration_machine.to_dict()
     payload = event_dict["payload"]
@@ -24,3 +25,26 @@ def validate_register(registration_machine):
 
     else:
         return True
+
+
+def validate_register_contains_not_empty_id(registration_machine):
+
+    event_dict = registration_machine.to_dict()
+    id_machine = event_dict["id_machine"]
+
+    if id_machine == "":
+        return False
+    elif id_machine == " ":
+        return False
+
+
+def validate_id_not_existing(event, repositories):
+
+    event_dict = event.to_dict()
+    id_machine = event_dict["id_machine"]
+    event_by_id = repositories["event"].get_event_by_machine_id(id_machine)
+
+    if event_by_id == None:
+        return True
+    else:
+        return False
