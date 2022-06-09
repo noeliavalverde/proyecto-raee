46e9ecceadf59e97cb18663482462b5dae60770e
noeliavm <noelia.valverde@gmail.com>
2022-06-09 10:34:24 +0200

    FEAT - Método para obtener estado actual de una máquina

---
+++ back/src/domain/projections/system.py
@@ -0,0 +1,7 @@
+def system_current_state(repository):
+    events = repository.get_events()
+    state = {"registered": 0, "repair_in": 0}
+    for event in events:
+        if event.event == "repair_in":
+            state["repair_in"] += 1
+    return state
